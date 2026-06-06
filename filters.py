b6ed40c434f5138f09bf9fa6bc39f88a4cacc152
Seshu K <seshubabu.kalakurthi@gmail.com>
2026-06-06 10:10:32 +0100

    updated filters

--- filters.py
+++ filters.py
@@ -120,7 +120,7 @@ def filter_dublin_only(jobs: List[Job]) -> List[Job]:
     """
     dublin_keywords = ["dublin", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9",
                        "d10", "d11", "d12", "d13", "d14", "d15", "d16", "d17", "d18",
-                       "d20", "d24", "dun laoghaire", "blackrock", "dalkey", "sandycove"]
+                       "d20", "d24"]
 
     dublin = []
     non_dublin = []
--- generate_dashboard.py
+++ generate_dashboard.py
@@ -19,7 +19,7 @@ def is_dublin(location: str) -> bool:
     loc = location.lower()
     dublin_keywords = ["dublin", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9",
                        "d10", "d11", "d12", "d13", "d14", "d15", "d16", "d17", "d18",
-                       "d20", "d24", "dun laoghaire", "blackrock", "dalkey", "sandycove"]
+                       "d20", "d24"]
     return any(keyword in loc for keyword in dublin_keywords)
 
 
